cc3bccf96a455cb6e957d6db24033e01cd7d5e1c
MuqadisaEjaz <i202301@nu.edu.pk>
2024-03-30 22:53:56 +0500

    checking

--- model/train_model.py
+++ model/train_model.py
@@ -14,7 +14,10 @@ X = data.drop(columns=['target'])
 y = data['target']
 
 # Split data into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
 
 # Train a logistic regression model
 model = LogisticRegression()
